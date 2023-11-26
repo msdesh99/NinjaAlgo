957aea66d60650e1a957b2391915efe7e032a720
Mdeshpande <msdesh99@gmail.com>
2023-11-26 06:59:00 -0500

    commit for allure result

--- ninjaalgoproject/ target/cucumber/datachrome/report.js
+++ ninjaalgoproject/ target/cucumber/datachrome/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"5bcfb128-fe0f-4661-8e7d-faaa44ad57d3","feature":"ArrayParallel module","scenario":"ArrayClass","start":1700962263660,"group":162,"content":"","tags":"@arrayclass,","end":1700962284352,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":162,"content":"Thread[#162,TestNG-test=Test Data-2,5,main]"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"64a70e3a-7df8-4fa9-b9c8-510122fef21c","feature":"ArrayParallel module","scenario":"ArrayClass","start":1700999577287,"group":161,"content":"","tags":"@arrayclass,","end":1700999595121,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":161,"content":"Thread[#161,TestNG-test=Test Data-2,5,main]"}]);
 });
